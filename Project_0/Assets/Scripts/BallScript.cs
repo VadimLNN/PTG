084a96f644807736fb109321561265fcd457b695
VadimLNN <svobodolubsev@inbox.ru>
2024-07-14 14:35:31 +0700

    сделал выделение красным при столкновении

--- Project_0/Assets/Scripts/BallScript.cs
+++ Project_0/Assets/Scripts/BallScript.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
+    [Range(0.1f, 100f)]
+    public float speed = 2;
+    public Material hitMat;
+    private Renderer ren;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ren = GetComponent<Renderer>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ren.material = hitMat; // смена материала при столкновении 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.right * Time.deltaTime * 2;
+        transform.position += transform.right * Time.deltaTime * speed;
     }
 }
