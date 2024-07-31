61d85cec7426ed4654a27e6b44eac6dd41431b76
VadimLNN <svobodolubsev@inbox.ru>
2024-07-31 18:34:22 +0700

    заставил ногу прикольно двигаться но пока моментально

---
+++ Project_4/Assets/Scripts/ControllerScr.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScr : MonoBehaviour
+{
+    // скорость перемещения и сила прыжка
+    public float speed = 4;
+    public float jumpForce = 500;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+    }
+
+    void LateUpdate()
+    {
+        // получения направления движения по клавишам  
+        float v = Input.GetAxisRaw("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        
+        Vector3 dir = new Vector3 (h, 0, v);
+        dir = transform.TransformDirection(dir.normalized); // применения поворота объекта к вектору направления движения 
+        dir *= speed;                                       // применение скорости к вектору направления
+        dir.y = rb.velocity.y;                              // восстановление смещения по оси Y
+        rb.velocity = dir;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // придание импульса вверх
+            rb.AddForce(Vector3.up * jumpForce);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            // поворот объекта против часовой 
+            transform.Rotate(Vector3.up, -Mathf.PI/8, Space.World);
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            // поворот объекта по часовой 
+            transform.Rotate(Vector3.up, Mathf.PI / 8, Space.World);
+        }
+    }
+}
--- Project_4/Assets/Scripts/legScr.cs
+++ Project_4/Assets/Scripts/legScr.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class legScr : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform projector;
+    public LayerMask ground;
+    float stepDistance = 0.2f;
+
+    Vector3 oldPos, newPos, currentPos;
+
+    RaycastHit hit;
+
+    float lerp = 1;
+
     void Start()
     {
-        
+        Physics.Raycast(projector.position, Vector3.down, out hit, 100, ground);
+        oldPos = newPos = currentPos = hit.point;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.position = oldPos;
+
+        Vector3 proj1 = Vector3.ProjectOnPlane(projector.position, Vector3.up);
+        Vector3 proj2 = Vector3.ProjectOnPlane(oldPos, Vector3.up);
+
+        if (Vector3.Distance(proj1, proj2) > stepDistance) 
+        {
+            Physics.Raycast(projector.position, Vector3.down, out hit, 100, ground);
+            oldPos = hit.point;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(projector.position, Vector3.down * 0.25f);
+        Gizmos.DrawWireSphere(transform.position, stepDistance);
     }
 }
