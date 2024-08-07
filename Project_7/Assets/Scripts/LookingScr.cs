5e90825247ed22dbe5ac94d0762b6f475ee84c89
VadimLNN <svobodolubsev@inbox.ru>
2024-08-07 16:18:32 +0700

    добавил фиксацию курсора

--- Project_7/Assets/Scripts/LookingScr.cs
+++ Project_7/Assets/Scripts/LookingScr.cs
@@ -1,12 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class LookingScr : MonoBehaviour
 {
-    // ссылки на игрока и камеру
+    // ссылки на игрока, камеру и фиксатор курсора
     public Transform player;
     public Transform cam;
+    public static bool cursorLock = true;
 
     // чувствительность мыши
     [Range(50f, 100f)]
@@ -27,16 +26,33 @@ public class LookingScr : MonoBehaviour
         // получение поворота по X
         float mouseY = Input.GetAxis("Mouse Y") * ySens * Time.deltaTime;
         Quaternion yRot = cam.localRotation * Quaternion.AngleAxis(mouseY, -Vector3.right);
-    
+
         // ограничение поворота на более чем 90 градусов 
         if (Quaternion.Angle(center, yRot) < 90f)
             cam.localRotation = yRot;
-        
+
         // получение поворота по Y 
         float mouseX = Input.GetAxis("Mouse X") * xSens * Time.deltaTime;
-        Quaternion xRot = cam.localRotation * Quaternion.AngleAxis(mouseX, Vector3.up);
-    
+        Quaternion xRot = player.localRotation * Quaternion.AngleAxis(mouseX, Vector3.up);
+
         // поворот игрока 
         player.localRotation = xRot;
+
+
+
+        if (cursorLock)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            if (Input.GetKeyDown(KeyCode.L))
+                cursorLock = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            if (Input.GetKeyDown(KeyCode.L))
+                cursorLock = true;
+        }
     }
 }
