e6d949bb954f78d5319df4c3bb37292b3f891769
VadimLNN <svobodolubsev@inbox.ru>
2024-08-07 16:29:17 +0700

    добавил sprint

--- Project_7/Assets/Scripts/MovementScr.cs
+++ Project_7/Assets/Scripts/MovementScr.cs
@@ -16,6 +16,8 @@ public class MovementScr : MonoBehaviour
 
     void FixedUpdate()
     {
+        bool sprint = (Input.GetKey(KeyCode.LeftShift));
+
         // проверка нажатий w a s d
         float xMove = Input.GetAxisRaw("Horizontal");
         float zMove = Input.GetAxisRaw("Vertical");
@@ -25,8 +27,14 @@ public class MovementScr : MonoBehaviour
         // нормализация вектора 
         dir.Normalize();
 
+        Vector3 v;
+
         // вектор вкорости = направление * скорость * время с прошедшего вызова
-        Vector3 v = transform.TransformDirection(dir) * mSpeed * Time.fixedDeltaTime;
+        if (!sprint)
+            v = transform.TransformDirection(dir) * mSpeed * Time.fixedDeltaTime;
+        else
+            v = transform.TransformDirection(dir) * mSpeed*2 * Time.fixedDeltaTime;
+
         // восстановление смещения по Y
         v.y = rb.velocity.y;
         // применение скорости
