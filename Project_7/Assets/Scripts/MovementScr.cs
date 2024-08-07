fe5d21f4b1b585f9e721fd15231a618727314f56
VadimLNN <svobodolubsev@inbox.ru>
2024-08-07 15:49:57 +0700

    прописал перемещение

--- Project_7/Assets/Scripts/MovementScr.cs
+++ Project_7/Assets/Scripts/MovementScr.cs
@@ -4,16 +4,32 @@ using UnityEngine;
 
 public class MovementScr : MonoBehaviour
 {
+    [Range(200f, 500f)]
+    public float mSpeed = 300f;
 
-
+    Rigidbody rb;
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        // проверка нажатий w a s d
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        // получения направления движения в плоскости X/Z
+        Vector3 dir = new Vector3(xMove, 0, zMove);
+        // нормализация вектора 
+        dir.Normalize();
+
+        // вектор вкорости = направление * скорость * время с прошедшего вызова
+        Vector3 v = transform.TransformDirection(dir) * mSpeed * Time.fixedDeltaTime;
+        // восстановление смещения по Y
+        v.y = rb.velocity.y;
+        // применение скорости
+        rb.velocity = v;
     }
 }
