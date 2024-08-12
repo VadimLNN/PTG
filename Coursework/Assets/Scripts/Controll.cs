dcc6838b752ff20b3897ab91817b969a9e3a4c1c
VadimLNN <svobodolubsev@inbox.ru>
2024-08-13 03:03:42 +0700

    настроил ходьбу вперёд и назад настроил бег вперёд и назад

--- Coursework/Assets/Scripts/Controll.cs
+++ Coursework/Assets/Scripts/Controll.cs
@@ -1,18 +1,124 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Controll : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // ссылки на физическое тело, состояние и аниматора
+    Rigidbody rb;
+    int state = 0;
+    Animator anim;
+
+    // скорость передвижения и поворота
+    float speed = 3;
+    public float ang_speed = 72;
+
+    // Характеристики для прыжка
+    [Range(1f, 10f)]
+    public float jumpForce = 0.35f;
+    public bool onGround;
+
+    // состояние атаки и полёта
+    bool attacking = false;
+    bool flying = false;
+
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        onGround = true;
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+            onGround = true;
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+            onGround = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        // установка анимации простоя
+        state = 0;
+
+        // если нет анимации атаки
+        if (attacking == false)
+        {
+            // установка анимации и передвижения вперёд, назад 
+            if (Input.GetAxisRaw("Vertical") > 0)
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    state = 11;                                                      // умножение скорости X2 тк бег
+                    rb.MovePosition(transform.position + transform.forward * Time.fixedDeltaTime * speed * 2);
+                }
+                else
+                {
+                    state = 1;
+                    rb.MovePosition(transform.position + transform.forward * Time.fixedDeltaTime * speed);
+                }
+            }
+            if (Input.GetAxisRaw("Vertical") < 0)
+            {
+                if (Input.GetKey(KeyCode.LeftShift))
+                {
+                    state = 22;                                                         // снижение скорости тк движение назад
+                    rb.MovePosition(transform.position + transform.forward * Time.fixedDeltaTime * speed/1.5f);
+                }
+                else
+                {
+                    state = 2;                                                           // снижение скорости тк движение назад
+                    rb.MovePosition(transform.position - transform.forward * Time.fixedDeltaTime * (speed / 2));
+                }
+            }
+
+            // повороты в стороны
+            if (Input.GetAxisRaw("Horizontal") > 0)
+            {
+                Quaternion deltaRotation = Quaternion.Euler(Vector3.up * Time.deltaTime * ang_speed); ;
+                rb.MoveRotation(rb.rotation * deltaRotation);
+            }
+            if (Input.GetAxisRaw("Horizontal") < 0)
+            {
+                Quaternion deltaRotation = Quaternion.Euler(Vector3.up * Time.deltaTime * -ang_speed);
+                rb.MoveRotation(rb.rotation * deltaRotation);
+            }
+
+            // атака мечом правой на пкм, если персонаж стоит, идёт вперёд/назад, бежит
+            if (onGround == true && (state == 0 || state == 1 || state == 2 || state == 3) && Input.GetAxis("Fire1") == 1)
+                state = 5;
+
+            // прыжок (взлёт)
+            if (onGround == true && Input.GetKey(KeyCode.Space))
+            {
+                state = 4;
+                rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            }
+            // полёт
+            if (onGround == false)
+            {
+                state = 7;
+                flying = true;
+            }
+            // приземление
+            if (onGround == true && flying == true)
+            {
+                state = 8;
+                flying = false;
+            }
+        }
+
+        // воспроизведение анимации
+        anim.SetInteger("state", state);
+    }
+
+    public void AttackOn()
+    {
+        attacking = true;
+    }
+    public void AttackOff()
+    {
+        attacking = false;
     }
 }
