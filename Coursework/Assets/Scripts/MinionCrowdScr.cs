879473f9ed2fb0f8dd0ad6e0ce89f0c7a0a1908e
VadimLNN <svobodolubsev@inbox.ru>
2024-08-19 21:05:45 +0700

    добавил проверуку на изменение и динамическое подстраивание мест при изменении их кол-ва

--- Coursework/Assets/Scripts/MinionCrowdScr.cs
+++ Coursework/Assets/Scripts/MinionCrowdScr.cs
@@ -17,23 +17,36 @@ public class MinionCrowd : MonoBehaviour
 
     void Start()
     {
-        for(int i = 0; i < transform.childCount; i++)
-            minions.Add(transform.GetChild(i));
+        
     }
 
     void Update()
     {
+        // переменная для отслеживания изменений колличества приспешников
+        int newCrowdCount = transform.childCount;
 
+        // перепись миньёнов в списке при изменении их колличества
+        if (newCrowdCount != crowdCount)
+        {
+            minions.Clear();
+            for(int i = 0; i < transform.childCount; i++)
+                minions.Add(transform.GetChild(i));
+        }
         
-
+        // число миньёнов
         crowdCount = minions.Count;
 
+        //
         List<Vector3> places = new List<Vector3>();
 
+        //
         playerPos = player.transform.position - player.transform.forward*0.7f;
+        
+        //
         float indentDown = 0.4f;
         float indentRight = 0.42f;
 
+        //
         int previousRow = 0;
         int currentRow = 0;
         int currentMinions = 0;
@@ -46,29 +59,26 @@ public class MinionCrowd : MonoBehaviour
                 for (float j = -(currentRow - 1 ) / 2f; j <= (currentRow - 1) / 2f; j++)
                     places.Add(playerPos - player.transform.forward * indentDown * currentRow
                                                     + player.transform.right * indentRight * j);
-                    /*Gizmos.DrawWireSphere(playerPos - player.transform.forward * indentDown * currentRow
-                                                    + player.transform.right * indentRight * j, 0.2f);*/
             else
             {
                 currentRow = crowdCount - currentMinions;
                 for (float j = -(currentRow - 1) / 2f; j <= (currentRow - 1) / 2f; j++)
                     places.Add(playerPos - player.transform.forward * indentDown * (previousRow + 1)
                                                     + player.transform.right * indentRight * j);
-                /*Gizmos.DrawWireSphere(playerPos - player.transform.forward * indentDown * (previousRow+1)
-                                                + player.transform.right * indentRight * j, 0.2f);*/
             }
             currentMinions += currentRow;
             previousRow = currentRow;
         }
 
+        // расстановка прихвостней по местам
         for (int i = 0; i < crowdCount; i++)
         {
-            Debug.Log(minions[i].name);
             minions[i].position = places[i];
-            Debug.Log(places[i]);
         }
 
     }
+
+
     
     private void OnDrawGizmos()
     {
@@ -97,12 +107,6 @@ public class MinionCrowd : MonoBehaviour
             }
             currentMinions += currentRow;
             previousRow = currentRow;
-
-           
         }
-
-
-
-
     }
 }
