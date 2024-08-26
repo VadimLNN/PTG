fe9a8f90fdb51ecc8590b169ff96be878fe96662
VadimLNN <svobodolubsev@inbox.ru>
2024-08-26 13:53:19 +0700

    прописал спавнер мобов

--- Coursework/Assets/Scripts/MobsSpawner.cs
+++ Coursework/Assets/Scripts/MobsSpawner.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class MobsSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // ссылки на префабы мобов
+    public GameObject[] mobs;
+
+    // зона спавна
+    public Vector3 spawnValue;
+    
+    // время спавна, максимальное и минимальное  
+    public float spawnWait;
+    public float spawnMostWait;
+    public float spawnLeastWait;
+    
+    // время до начала работы спавнера
+    public int startWait;
+
+    // состояние спавна 
+    public bool stop;
+
     void Start()
     {
-        
+        StartCoroutine(waitSpawner());
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+    IEnumerator waitSpawner()
+    {
+        yield return new WaitForSeconds(startWait);
+
+        while (!stop)
+        {
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), 1, Random.Range(-spawnValue.z, spawnValue.z));
+
+            Instantiate(mobs[Random.Range(0, mobs.Length-1)], spawnPos + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
     }
 }
