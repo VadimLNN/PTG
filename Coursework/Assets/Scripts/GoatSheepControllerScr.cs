7e163deb30b9a24136dfcdea4310142e47bb3cec
VadimLNN <svobodolubsev@inbox.ru>
2024-08-28 18:39:23 +0700

    добавил убиваемость овцам

--- Coursework/Assets/Scripts/GoatSheepControllerScr.cs
+++ Coursework/Assets/Scripts/GoatSheepControllerScr.cs
@@ -14,6 +14,10 @@ public class GoatSheepControllerScr : MonoBehaviour
     // параметры состония
     int state;
 
+    // кол-во здоровья
+    public int hp = 15;
+
+    bool isDead = false;
 
     void Start()
     {
@@ -22,17 +26,33 @@ public class GoatSheepControllerScr : MonoBehaviour
 
     void Update()
     {
-        // отслеживание состояний простоя и ходьбы
-        newPos = transform.position;
-
-        if (oldPos == newPos)
-            state = 0;
-        else
+        if (hp <= 0)
+        {
+            //agent.SetDestination(transform.position);
             state = 1;
+            isDead = true;
+            Destroy(this.gameObject, 2);
+        }
 
-        oldPos = newPos;
+        if (isDead == false)
+        {
+            // отслеживание состояний простоя и ходьбы
+            newPos = transform.position;
+
+            if (oldPos == newPos)
+                state = 0;
+            else
+                state = 1;
+
+            oldPos = newPos;
+        }
 
         // установка анимации
         anim.SetInteger("state", state);
+    }
+
+    public void takeDamage(int gamage)
+    {
+        hp -= gamage;
     }
 }
