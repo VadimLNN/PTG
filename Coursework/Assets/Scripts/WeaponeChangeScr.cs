7c6fdbb0974d485dfc33bf0b9fbe331cb6f9cdfa
VadimLNN <svobodolubsev@inbox.ru>
2024-09-02 22:11:53 +0700

    прописал смену оружия на 1 2 3, но при повороте влево после смены начинает колбавить

--- Coursework/Assets/Scripts/WeaponeChangeScr.cs
+++ Coursework/Assets/Scripts/WeaponeChangeScr.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class WeaponeChangeScr : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject[] shields;
+    public GameObject[] swords;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            switchWeapon(1);
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+            switchWeapon(2);
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+            switchWeapon(3);
+    }
+
+    void switchWeapon(int num)
+    {
+        for (int i = 1; i <= shields.Length; i++) 
+        {
+            if (i == num)
+                shields[i - 1].SetActive(true);
+            else 
+                shields[i - 1].SetActive(false);
+
+            if (i == num)
+                swords[i - 1].SetActive(true);
+            else
+                swords[i - 1].SetActive(false);
+        }
+
+
     }
 }
