4960163413c9e8a2747f4ad8637133d9dcadb9e6
VadimLNN <svobodolubsev@inbox.ru>
2024-09-23 20:02:18 +0700

    скрипт подсчёта очков и времени

---
+++ Project_3.2/Assets/Scripts/ScoreScr.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+
+public class ScoreScr : MonoBehaviour
+{
+    public TMP_Text score_text;
+    public TMP_Text time_text;
+
+    public GameObject win_panel;
+    public TMP_Text final_time_text;
+
+    int score = 0;
+    float time = 0;
+
+    void Start()
+    {
+        scoreUpdate();
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        time_text.text = Mathf.Round(time).ToString();
+    }
+
+    public void scoreUp(int points)
+    {
+        score += points;
+        scoreUpdate();
+    }
+
+    public void scoreUpdate()
+    {
+        score_text.text = score.ToString();
+
+        if (score == 16)
+        {
+            Time.timeScale = 0;
+            win_panel.SetActive(true);
+            score = 0;
+            final_time_text.text = time.ToString();
+            time = 0;
+        }
+    }
+}
