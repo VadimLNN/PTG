20f01737d9289a571ffb77caf5ed8ef2cf2c6d5d
VadimLNN <svobodolubsev@inbox.ru>
2024-09-23 20:01:53 +0700

    полностью переписал скрипт цели

--- Project_3.2/Assets/Scripts/TargetScr.cs
+++ Project_3.2/Assets/Scripts/TargetScr.cs
@@ -1,79 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class TargetScr : MonoBehaviour
 {
-    // характеристики жизни, очков 
-    public float hp = 20f;
-    float score = 100;
-    float scale;
-
-    // ссылка на систему частиц взрыва при смерти, аниматор  
-    public ParticleSystem explosion;
     Animator anim;
 
-    // ссылка на оружие и скрипт для него 
-    public GunScr gunScr;
-
-    // продолжительность жизни
-    float lifeTime;
+    public float deletRate = 2f;
+    public float nextDelet = 0f;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-
-        // случайность решает сколько жить скелетончику,
-        // его размер и следовательно множитель очков
-        lifeTime = Random.Range(5f, 60f);
-        scale = Random.Range(0.5f, 1.5f);
-        
-        transform.localScale *= scale;
-        //transform.Rotate(Vector3.up, -Mathf.PI / 8, Space.World);
-
-        score /= scale;
     }
 
     private void Update()
     {
-        lifeTime -= Time.deltaTime;
-        if(lifeTime <= 0)
-            Death();
-
-        score -= Time.deltaTime;
+        if (transform.parent != null)
+            if (transform.parent.childCount > 2 && Time.time >= nextDelet)
+            {
+                nextDelet = Time.time + 1 / deletRate;
+                
+                Destroy(transform.parent.GetChild(1));
+            }
     }
 
-    public void Hit(float damage)
+    public void Hit()
     {
-        hp -= damage;
-        if (hp <= 0 && score > 0)
-        {
-            Death();
-            gunScr.GetScore(score);
-            score = -1;
-        }
-    }
+        GameObject scoreBoard = GameObject.FindWithTag("ScoreBoard");
+        if (scoreBoard != null)
+            scoreBoard.GetComponent<ScoreScr>().scoreUp(1);
 
-    void Death()
-    {
-        // запуск анимации смерти
-        anim.SetBool("isDead", true);
-        Invoke("Explosion", 0.4f);
-    }
-
-    void Explosion()
-    {
-        // создание копии эффекта взрыва и размещение её по координатам цели
-        ParticleSystem exp = Instantiate(explosion, transform.position, transform.rotation);
-        exp.transform.localScale *= scale;
-        // воспроизведение анимации 
-        exp.Play();
-        
-        // уничтожение системы частиц через секунду 
-        Destroy(exp.gameObject, 1f);
-        // уничтожение цели 
-        Destroy(gameObject);
+        GetComponent<MeshDestroy>().DestroyThis();
     }
 }
