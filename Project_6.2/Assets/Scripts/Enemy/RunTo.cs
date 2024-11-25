acdca768c8fa2c1245abb8149c110808f972d0e8
VadimLNN <svobodolubsev@inbox.ru>
2024-11-25 22:51:05 +0700

    добавление класса для перемещения

---
+++ Project_6.2/Assets/Scripts/Enemy/RunTo.cs
@@ -0,0 +1,24 @@
+public class RunTo : IState
+{
+    AbstractEnemy enemy;
+
+    public RunTo(AbstractEnemy enemy)
+    {
+        this.enemy = enemy;
+    }
+
+    public void enter()
+    {
+        enemy.stop(false);
+        enemy.moveTo(enemy.Player.position);
+    }
+
+    public void exit()
+    {
+        enemy.stop(true);
+    }
+    public void update()
+    {
+        enemy.moveTo(enemy.Player.position);
+    }
+}
