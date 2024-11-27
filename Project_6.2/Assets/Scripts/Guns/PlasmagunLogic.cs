ad3e223cb78c34f7b5602665803aee840278f7e2
VadimLNN <svobodolubsev@inbox.ru>
2024-11-27 12:03:03 +0700

    работающий плазмаган

--- Project_6.2/Assets/Scripts/Guns/PlasmagunLogic.cs
+++ Project_6.2/Assets/Scripts/Guns/PlasmagunLogic.cs
@@ -6,8 +6,11 @@ public class PlasmagunLogic : MonoBehaviour
 {
     [SerializeField] LayerMask enemy;
 
+    public Transform shotPoint;
+    public GameObject projectile;
+
     public void shot(Transform firePoint, float damage)
     {
-        
+        Instantiate(projectile, shotPoint.position, shotPoint.rotation);
     }
 }
