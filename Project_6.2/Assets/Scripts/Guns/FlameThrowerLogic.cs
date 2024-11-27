cc534c0162596d388c8b9b012b98f8cf2a477b8d
VadimLNN <svobodolubsev@inbox.ru>
2024-11-27 14:09:22 +0700

    прописал огнемёт

--- Project_6.2/Assets/Scripts/Guns/FlameThrowerLogic.cs
+++ Project_6.2/Assets/Scripts/Guns/FlameThrowerLogic.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class FlameThrowerLogic : MonoBehaviour
 {
     [SerializeField] LayerMask enemy;
-    
+
+    public Transform shotPoint;
+    public GameObject projectile;
+
     public void shot(Transform firePoint, float damage)
     {
-        
+        Instantiate(projectile, shotPoint.position, shotPoint.rotation);
     }
 }
